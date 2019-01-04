7eb2b60d0ee5de7b3cff86b404c325f6864024d5
NoriKaneshige <atsunori@umich.edu>
2019-01-04 15:36:00 -0500

    2019/01/04 3:35PM

--- exercise_1_element_of_programming.java
+++ exercise_1_element_of_programming.java
@@ -397,14 +397,135 @@
 // 1.5 Input and Output
 // I copied and paste the codes from StdOut.java and StdIn.java from the website to create those files in the same directry.
 // Then, I became able to use StdIn and StdOut.
-public class exercise_1_element_of_programming {
-    public static void main(String[] args) {
-        int n = Integer.parseInt(args[0]);
-        int sum = 0;
-        for (int i = 0; i < n; i++) {
-            int value = StdIn.readInt();
-            sum = sum + value;
-        }
-        StdOut.println("Sum is " + sum);
-    }
-}
+// Compute Sum based on your input
+// public class exercise_1_element_of_programming {
+//     public static void main(String[] args) {
+//         int n = Integer.parseInt(args[0]);
+//         int sum = 0;
+//         for (int i = 0; i < n; i++) {
+//             int value = StdIn.readInt();
+//             sum = sum + value;
+//         }
+//         StdOut.println("Sum is " + sum);
+//     }
+// }
+
+
+// Range Filter
+/******************************************************************************
+ *  Compilation:  javac RangeFilter.java
+ *  Execution:    java RangeFilter lo hi < input.txt
+ *  Dependencies: StdIn.java StdOut.java
+ *
+ *  Read in a sequence of integers from standard input and print
+ *  out those values between lo and hi.
+ * java RandomSeq 5 | sort
+ ******************************************************************************/
+// public class exercise_1_element_of_programming {
+//     public static void main(String[] args) {
+//         // read in two command-line arguments
+//         int lo = Integer.parseInt(args[0]);
+//         int hi = Integer.parseInt(args[1]);
+//         // repeat as long as there's more input to read in
+//         while (!StdIn.isEmpty()) {
+//             // read in the next integer
+//             int t = StdIn.readInt();
+//             // print out the given integer if it's between lo and hi
+//             if (t >= lo && t <= hi) {
+//                 StdOut.print(t + " ");
+//             }
+//         }
+//         StdOut.println();
+//     }
+// }
+
+
+// Standard drawing
+// Triangle.java
+/******************************************************************************
+ *  Compilation:  javac Triangle.java
+ *  Execution:    java Triangle
+ *  Dependencies: StdDraw.java
+ *
+ *  Plot a triangle.
+ *
+ ******************************************************************************/
+// public class exercise_1_element_of_programming {
+//     public static void main(String[] args) {
+//         double t = Math.sqrt(3.0) / 2.0;
+//         StdDraw.line(0.0, 0.0, 1.0, 0.0);
+//         StdDraw.line(1.0, 0.0, 0.5, t);
+//         StdDraw.line(0.5, t, 0.0, 0.0);
+//         StdDraw.point(0.5, t/3.0);
+//     }
+// }
+
+// PlotFilter.java
+// I made USA.txt and store it in the same directly. Then I copied and pasted the content to it.
+/******************************************************************************
+ *  Compilation:  javac PlotFilter.java
+ *  Execution:    java PlotFilter < input.txt
+ *  Dependencies: StdDraw.java StdIn.java
+ *
+ *  % java PlotFilter < USA.txt
+ *
+ *  Datafiles:    http://introcs.cs.princeton.edu/java/15inout/USA.txt
+ *
+ ******************************************************************************/
+// public class exercise_1_element_of_programming {
+//
+//     public static void main(String[] args) {
+//
+//         // read in bounding box and rescale
+//         double x0 = StdIn.readDouble();
+//         double y0 = StdIn.readDouble();
+//         double x1 = StdIn.readDouble();
+//         double y1 = StdIn.readDouble();
+//         StdDraw.setXscale(x0, x1);
+//         StdDraw.setYscale(y0, y1);
+//
+//         // for bigger points
+//         StdDraw.setPenRadius(0.005);
+//
+//         // to speed up performance, defer displaying points
+//         StdDraw.enableDoubleBuffering();
+//
+//         // plot points, one at a time
+//         while (!StdIn.isEmpty()) {
+//             double x = StdIn.readDouble();
+//             double y = StdIn.readDouble();
+//             StdDraw.point(x, y);
+//         }
+//
+//         // display all of the points now
+//         StdDraw.show();
+//
+//     }
+// }
+
+// Checker Boad
+/******************************************************************************
+ *  Compilation:  javac Checkerboard.java
+ *  Execution:    java Checkerboard n
+ *  Dependencies: StdDraw.java
+ *
+ *  Plots an n-by-n checkerboard.
+ *
+ ******************************************************************************/
+
+// public class exercise_1_element_of_programming {
+//
+//     public static void main(String[] args) {
+//         int n = Integer.parseInt(args[0]);
+//         StdDraw.setXscale(0, n);
+//         StdDraw.setYscale(0, n);
+//
+//         for (int i = 0; i < n; i++) {
+//             for (int j = 0; j < n; j++) {
+//                 if ((i + j) % 2 != 0) StdDraw.setPenColor(StdDraw.BLACK);
+//                 else                  StdDraw.setPenColor(StdDraw.RED);
+//                 StdDraw.filledSquare(i + 0.5, j + 0.5, 0.5);
+//             }
+//         }
+//     }
+// }
